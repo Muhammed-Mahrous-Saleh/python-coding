e65c17152672310b6a78dce51fa6d9bf399c1016
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2023-07-15 08:47:05 +0300

    print combo number

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -6,4 +6,4 @@ for i in range(10):
         elif i == 8 and j == 9:
             print("{:d}{:d}".format(i, j))
             break
-        print("{:d}{:d}".format(i, j),end=", ")
+        print("{:d}{:d}".format(i, j), end=", ")
